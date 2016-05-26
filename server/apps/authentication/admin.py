622dd9b4d64010a7e265c2d8371d2e6644617ef0
David Karchmer <dkarchmer@gmail.com>
2016-05-25 22:57:03 -0700

    Add account admin custom forms

--- server/apps/authentication/admin.py
+++ server/apps/authentication/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from .models import *
 
+from .forms import AdminUserCreationForm, AdminUserChangeForm
+
 class AccountAdmin(admin.ModelAdmin):
+
+    def get_form(self, request, obj=None, **kwargs):
+
+        if obj:
+            return AdminUserChangeForm
+        else:
+            return AdminUserCreationForm
+
     list_display = ('id', 'username', 'email', 'name', 'created_at', 'last_login', 'is_active', 'is_staff')
+
 
 
 """
